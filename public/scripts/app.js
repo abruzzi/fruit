b611b5c9633cfedbbd42e36a1ea048db22058445
Sarah <sarah.sun422@gmail.com>
2015-03-25 14:03:10 +0800

    [Sarah] add js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,14 @@
 $(function(){
     $('.submit').click(function() {
-
-        
+        $.ajax({
+            type: "POST",
+            url: "/",
+            data: $("#message").val(),
+            success: function(data) {
+                alert(data);
+            },
+            dataType: "text"
+        });
     });
 
 });
